350db6afa8a1307278eb2852baa9bfa6c4b894cd
SirAdan <lpelcastre@bomuca.com>
2022-12-01 12:29:59 -0800

    add btn

--- PracticeMUI/src/App.jsx
+++ PracticeMUI/src/App.jsx
@@ -1,17 +1,35 @@
-import reactLogo from './assets/react.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Typography } from '@mui/material';
+import { makeStyles } from '@mui/material';
+
+const useStyle = makeStyles({
+  customBtn: {
+    height: 50,
+    color: '#f7f7f7',
+    background: '#2a2b2c',
+  },
+});
 
 const App = () => {
+  const classes = useStyle();
+
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Button variant="contained" color="secondary" disableElevation>
+        Color
+      </Button>
+      <Button variant="outlined" color="primary" startIcon={<DeleteIcon />}>
+        Borrar
+      </Button>
+      <Typography variant="h4" paragraph>
+        texto
+      </Typography>
+      <Typography align="right" variant="body1" gutterBottom>
+        body, body,, bodai!
+      </Typography>
+      <Button className={classes.customBtn}>Boton Personalizado</Button>
     </div>
   );
 };
